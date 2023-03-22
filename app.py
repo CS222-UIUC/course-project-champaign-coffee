ea09965b2939fede3fc876a3332036fda707a40b
Monica <mpara3@illinois.edu>
2023-03-22 14:43:57 -0500

    setting up frontend

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """ FLASK SERVER FOR WEB APP """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 """ type 'flask run' in terminal to run local server to http://127.0.0.1:5000/ """
 @app.route("/")
 def hello():
     """ default server """
-    return "Hello Champaign Coffee!"
+    return render_template('index.html')
