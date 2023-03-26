ab0c5bc8927edd566c69c259a5f7318b86637e42
Monica <mpara3@illinois.edu>
2023-03-26 18:31:16 -0500

    implemented UI for all 4 pages

--- app.py
+++ app.py
@@ -2,7 +2,20 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 """ type 'flask run' in terminal to run local server to http://127.0.0.1:5000/ """
+
 @app.route("/")
-def hello():
+def home():
     """ default server """
     return render_template('index.html')
+
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')
+
+@app.route('/ratings')
+def ratings():
+    return render_template('ratings.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
