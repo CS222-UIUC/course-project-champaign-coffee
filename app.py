df164b80d14c146b3c900e9569cf53fc3c24e0a6
Minhyung-Lee <ml90@illinois.edu>
2023-04-05 22:05:03 -0500

    Add ratings update feature, etc
    the user submission of ratings now directly updates the sql db (app.py now imports db and uses session query and uses db's fxns to directly update the sql db and commits right after)
    set foundation down for price input in app.py route /discover

--- app.py
+++ app.py
@@ -1,40 +1,80 @@
 """ FLASK SERVER FOR WEB APP """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from db import CoffeeShop, Item, CoffeeData, session
+
 app = Flask(__name__)
 app_version = '1.0'
+
 
 @app.route("/")
 def home():
     """ default server """
     return render_template('index.html')
 
+
 @app.route('/discover')
 def discover():
+   #  price1 = request.form['price1']
+   #  price2 = request.form['price2']
+
+   #  try:
+   #      price1 = float(price1)
+   #      price2 = float(price2)
+   #      rating = float(rating)
+   #  except ValueError:
+   #      return render_template('ratings.html', error="Invalid input for price range.")
+
+   #  if price1 < 0 or price2 < 0:
+   #      return render_template('ratings.html', error="Price range must be non-negative.")
+
+   #  if price1 > price2:
+   #      return render_template('ratings.html', error="Minimum price cannot be greater than maximum price.")
+
+   #   #local testing
+   #  print(f"Price range: {price1}-{price2}")
     return render_template('discover.html')
+
 
 @app.route('/ratings')
 def ratings():
     return render_template('ratings.html')
 
+
 @app.route('/submit_rating', methods=['POST'])
 def submit_rating():
-    coffee_shop = request.form['coffee_shop']
+    coffee_shop_name = request.form['coffee_shop']
     rating = request.form['rating']
     feedback = request.form['feedback']
 
-     # local testing
+    coffee_shop = session.query(CoffeeShop).filter(
+        CoffeeShop.name == coffee_shop_name).first()
+
+    if not coffee_shop:
+        return render_template('ratings.html', error="Invalid coffee shop name.")
+
+    coffee_shop.update_ratings(float(rating))
+   #  session.add(self)
+    session.commit()
+
+    # local testing
     print(f"Coffee shop: {coffee_shop}")
     print(f"Rating: {rating}/5")
-    print(f"Feedback: {feedback}") 
+    print(f"Feedback: {feedback}")
 
     return render_template('ratings.html', submitted=True)
+
 
 @app.route('/feedback')
 def feedback():
     return render_template('feedback.html')
 
+
 @app.route('/submit-feedback', methods=['POST'])
 def submit_feedback():
-    feedback = request.form.get('textbox') 
-    print(f"New feedback received: {feedback}") 
+    feedback = request.form.get('textbox')
+    print(f"New feedback received: {feedback}")
     return render_template('feedback.html', feedback=feedback)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
